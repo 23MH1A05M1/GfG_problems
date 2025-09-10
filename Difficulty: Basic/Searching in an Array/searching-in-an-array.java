4eb264ab32014e8f5241b5bd7e43bab31585226d
Bhagyasri <147595512+23MH1A05M1@users.noreply.github.com>
2025-09-10 23:20:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Searching in an Array/searching-in-an-array.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+class Solution {
+    public int search(int k, ArrayList<Integer> arr) {
+        // code here
+        int res=-1;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr.get(i)==k)
+            {
+                res=i+1;
+                break;
+            }
+        }
+        return res;
+    }
+}
